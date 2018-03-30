8cd3a35ade2fe07450d633e2504c31afea9428d9
Zihan Chen <zihan.chen.jhu@gmail.com>
2018-03-30 12:55:29 -0700

    Added matplotlib basics

---
+++ python/mpl/mpl_basics.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+"""
+Basic Matplotlib examples shows:
+- create figure, subplots
+- create axes
+- x/y label
+- x/y limit
+- set ticks
+- legend
+- grid
+"""
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+fig = plt.figure()
+
+rows = 1
+cols = 2
+ax1 = fig.add_subplot(rows, cols, 1)
+ax2 = fig.add_subplot(rows, cols, 2)
+
+
+x = np.linspace(0, 10, 1000)
+y_sin = np.sin(x)
+y_sin_2 = np.sin(x) * 0.5
+y_cos = np.cos(x)
+
+ax1.plot(x, y_sin)
+ax1.plot(x, y_sin_2)
+ax2.plot(x, y_cos)
+
+# set title
+fig.suptitle('Sup title')
+ax1.set_title('Ax1 title sine')
+ax2.set_title('Ax2 title cosine')
+
+# set label
+ax1.set_xlabel('x data')
+ax1.set_ylabel('y data sin(x)')
+
+# set limits
+ax2.set_xlabel('x data')
+ax2.set_ylabel('y data cos(x)')
+
+# set ticks
+ax2.set_xticks(range(0, 11))
+
+# set legend
+# see legend documentation for details
+ax1.legend(['sin', 'sin * 0.5'], loc='upper center')
+
+# set grid
+ax1.grid(True)
+ax2.grid(True)
+
+plt.show()
+
+
+
