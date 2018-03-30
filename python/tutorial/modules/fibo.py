5e5a7cea4dea58fc4d53523dbba1e344e6d32fcb
Zihan Chen <zihan.chen.jhu@gmail.com>
2018-03-30 12:55:57 -0700

    Updated fibo to support python3

--- python/tutorial/modules/fibo.py
+++ python/tutorial/modules/fibo.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
-# fibonacci number
-def fib(n):    # write Fibonacci series up to n
+from __future__ import print_function
+
+
+def fib(n):
+    # write Fibonacci series up to n
     a, b = 0, 1
     while b < n:
-        print b,
+        print(b)
         a, b = b, a+b
-        
-def fib2(n): # return Fibonacci series up to n
+
+
+def fib2(n):
+    # return Fibonacci series up to n
     result = []
     a, b = 0, 1
     while b < n:
@@ -15,7 +20,7 @@ def fib2(n): # return Fibonacci series up to n
         a, b = b, a+b
     return result
 
+
 if __name__ == "__main__":
     import sys
     fib(int(sys.argv[1]))
-
