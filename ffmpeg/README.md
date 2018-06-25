e763073bda8daa0c657d4e7dfec2a44c99be07a6
Zihan Chen <zihan.chen.jhu@gmail.com>
2018-06-24 22:27:45 -0700

    Started ffmpeg tutorial 02

--- ffmpeg/README.md
+++ ffmpeg/README.md
@@ -1,5 +1,5 @@
 # About
-The codes are from  on Michael Penkov's ffmpeg tutorial. See http://dranger.com/ffmpeg/
+The codes are from  on Michael Penkov's ffmpeg tutorial. See http://dranger.com/ffmpeg/ and https://github.com/mpenkov/ffmpeg-tutorial
 
 # Tested on 
 Ubuntu 16.04 with ffmpeg 2.8 and SDL 2.0.4 
--- ffmpeg/tutorial_02.cpp
+++ ffmpeg/tutorial_02.cpp
@@ -1,83 +1,163 @@
-// Code is based on the following tutorial
-// http://dranger.com/ffmpeg/tutorial03.html
+// Code is based on the following tutorial 02
+// http://dranger.com/ffmpeg/tutorial02.html
 // 2018-06-24
 //
-// OS: Ubuntu 16.04
-// ffmpeg version: 2.8
-// SDL version: 2.0.5
+// Covers the following items:
 
 
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_thread.h>
+
 extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
 }
 
-
-int main(int argc, char** argv) {
-    std::cout << "Hello, World!" << std::endl;
-    int ret;
-    AVFormatContext* pFormatCtx;
+int main(int argc, char *argv[])
+{
+    std::cout << "hello\n";
 
     av_register_all();
-    
 
+    AVFormatContext *pFormatCtx = nullptr;
+    if (avformat_open_input(&pFormatCtx, argv[1], nullptr, nullptr) != 0) {
+        std::cerr << "Failed to open file\n";
+        return -1;
+    }
+
+    if (avformat_find_stream_info(pFormatCtx, nullptr) < 0) {
+        std::cerr << "Failed to find stream info\n";
+        return -1;
+    }
+
+    AVCodecContext *pCodecCtx = nullptr;
+    AVCodecContext *pCodecCtxOriginal = nullptr;
     int videoStream = -1;
-    int audioStream = -1;
-
     for (int i = 0; i < pFormatCtx->nb_streams; i++) {
-        if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO &&
-            videoStream < 0)
-        {
+        if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
             videoStream = i;
-        }
-        if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO &&
-            audioStream < 0)
-        {
-            audioStream = i;
+            break;
         }
     }
-    if (videoStream < 0) {
-        std::cerr << "Failed to find a video stream\n";
+    if (videoStream == -1) {
+        std::cerr << "Failed to find a VIDEO stream\n";
         return -1;
+    } else {
+        std::cout << "Video stream = " << videoStream << "\n";
     }
-    if (audioStream < 0) {
-        std::cerr << "Failed to find an audio stream\n";
-        return -1;
-    }
+    pCodecCtxOriginal = pFormatCtx->streams[videoStream]->codec;
 
-    // -----------------------------
-    // Get AVCodecContext
-    // -----------------------------
-    AVCodecContext *aCodecCtxOrig;
-    AVCodecContext *aCodecCtx;
-    AVCodec *aCodec;
-
-    aCodecCtxOrig = pFormatCtx->streams[audioStream]->codec;
-    aCodec = avcodec_find_decoder(aCodecCtxOrig->codec_id);
-    if (!aCodec) {
+    AVCodec *pCodec = nullptr;
+    pCodec = avcodec_find_decoder(pCodecCtxOriginal->codec_id);
+    if (pCodec == nullptr) {
         std::cerr << "Unsupported codec!\n";
         return -1;
     }
 
-    // Copy content
-    aCodecCtx = avcodec_alloc_context3(aCodec);
-    if (avcodec_copy_context(aCodecCtx, aCodecCtxOrig) != 0) {
-        std::cerr << "Failed to copy codec context\n";
+    // Copy Context
+    pCodecCtx = avcodec_alloc_context3(pCodec);
+    if (avcodec_copy_context(pCodecCtx, pCodecCtxOriginal) != 0) {
+        std::cerr << "Failed to copy Codec Context\n";
         return -1;
     }
 
-    if (avcodec_open2(aCodecCtx, aCodec, nullptr) < 0) {
-        std::cerr << "Failed to open codec\n";
+    // Open Codec
+    if (avcodec_open2(pCodecCtx, pCodec, nullptr) < 0) {
+        std::cerr << "Failed to open Codec\n";
+        return -1;
     }
 
-    SDL_AudioSpec desired_audio_spec;
-    SDL_AudioSpec audio_spec;
-
-    if (SDL_OpenAudio(&desired_audio_spec, &audio_spec) < 0) {
-        std::cerr << "Failed: SDL OpenAudio. " << SDL_GetError() << "\n";
+    AVFrame* pFrame;
+    AVFrame* pFrameBGR;
+    pFrame = av_frame_alloc();
+    pFrameBGR = av_frame_alloc();
+    if (pFrame == nullptr || pFrameBGR == nullptr) {
+        std::cerr << "Failed to allocate frame\n";
+        return -1;
     }
 
+    int numFrameBytes;
+    numFrameBytes = avpicture_get_size(AV_PIX_FMT_BGR24, pCodecCtx->width, pCodecCtx->height);
+    avpicture_fill((AVPicture*)pFrameBGR,
+                   (uint8_t*)av_malloc(numFrameBytes * sizeof(uint8_t)),
+                   AV_PIX_FMT_BGR24,
+                   pCodecCtx->width,
+                   pCodecCtx->height);
+    std::cout << "Picture width = " << pCodecCtx->width << " height = " << pCodecCtx->height << "\n";
+
+    // Format Conversion
+    struct SwsContext * pSwsCtx = nullptr;
+    pSwsCtx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt,
+                             pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_BGR24,
+                             SWS_BILINEAR,  nullptr, nullptr, nullptr);
+    if (pSwsCtx == nullptr) {
+        std::cerr << "Failed to get the conversion context\n";
+    }
+
+
+
+    // ----------------------------------------
+    // Tutorial 02: Outputting to the Screen
+    // ----------------------------------------
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) < 0) {
+        std::cerr << "Could not initialize SDL " << SDL_GetError() << "\n";
+        return -1;
+    }
+
+    // -----------------------------
+    // Loop through the video file
+    // -----------------------------
+
+    int packet_index = 0;
+    AVPacket packet;
+    while (av_read_frame(pFormatCtx, &packet) >= 0) {
+        if (packet.stream_index == videoStream) {
+            packet_index++;
+
+            // convert to AVFrame
+            int got_picture;
+            if (avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, &packet) < 0) {
+                std::cerr << "Failed to decode video AVPacket\n";
+            }
+
+            if (got_picture) {
+                std::cout << "Successfully decoded a frame " << packet_index << "\n";
+
+                // convert to RGB format
+                sws_scale(pSwsCtx,
+                          pFrame->data,
+                          pFrame->linesize,
+                          0,
+                          pFrame->height,
+                          pFrameBGR->data,
+                          pFrameBGR->linesize);
+
+                cv::Mat mat(pCodecCtx->height, pCodecCtx->width, CV_8UC3, pFrameBGR->data[0]);
+                char filename[100];
+                sprintf(filename, "frame%02d.jpg", packet_index);
+                cv::imwrite(filename, mat);
+            }
+        }
+
+        // Free the packet that was allocated by av_read_frame
+        av_free_packet(&packet);
+
+        if (packet_index == 12) {
+            break;
+        }
+    }
+
+    // Free allocated resources
+    av_frame_free(&pFrame);
+    av_frame_free(&pFrameBGR);
+
+    // Close the codec
+    avcodec_close(pCodecCtx);
+    avcodec_close(pCodecCtxOriginal);
+
+    // Close the video file
+    avformat_close_input(&pFormatCtx);
     return 0;
 }
