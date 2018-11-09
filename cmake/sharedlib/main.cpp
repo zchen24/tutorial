b1c212559eaeacd7ee2d9c92f44abae5b05b0cb7
Zihan Chen <zihan.chen.jhu@gmail.com>
2018-11-09 14:55:03 -0800

    Added CMake shared library example

---
+++ cmake/sharedlib/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "mylib.h"
+
+int main()
+{
+    std::cout << "3 + 5 = " << add(3,5) << "\n";
+    return 0;
+}
---
+++ cmake/sharedlib/mylib.cpp
@@ -0,0 +1,6 @@
+#include "mylib.h"
+
+int add(int x, int y)
+{
+    return (x + y);
+}
