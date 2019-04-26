5af28a8948aa033e357cc265b3db7f3a3ed9f188
Zihan Chen <zihan.chen.jhu@gmail.com>
2019-04-26 07:53:42 -0700

    Refactored dist_v2_cuda

--- cuda/dist_v2_cuda/dist_v2_cuda.cu
+++ cuda/dist_v2_cuda/dist_v2_cuda.cu
@@ -1,10 +1,15 @@
 // From CUDA for Engineering
 // dist_v2/kernel.cu
 
-#include "dist_v2.h"
 #include <stdio.h>
 #include <cuda_runtime.h>
+#include <iostream>
+#include <iomanip>
+
 #define TPB 32
+#define N 10
+
+#define DEBUG 1
 
 __device__ 
 float distance(float x1, float x2)
@@ -13,9 +18,10 @@ float distance(float x1, float x2)
 }
 
 __global__
-void distanceKernel(float *d_out, float *d_in, float ref)
+void distanceKernel(float *d_out, float *d_in, float ref, int len)
 {
     const int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i >= len) { return; }
     const float x = d_in[i];
     d_out[i] = distance(x, ref);
 }
@@ -32,7 +38,7 @@ void distanceArray(float *out, float *in, float ref, int len)
     cudaMemcpy(d_in, in, len * sizeof(float), cudaMemcpyHostToDevice);
 
     // call wrapper
-    distanceKernel<<<len/TPB, TPB>>>(d_out, d_in, ref);
+    distanceKernel<<<(len+TPB-1)/TPB, TPB>>>(d_out, d_in, ref, len);
 
     // memcpy from device
     cudaMemcpy(out, d_out, len * sizeof(float), cudaMemcpyDeviceToHost);
@@ -40,4 +46,33 @@ void distanceArray(float *out, float *in, float ref, int len)
     // free cuda memory
     cudaFree(d_in);
     cudaFree(d_out);
+}
+
+float scale(int i, int n) {
+    return ((float)i) / (n - 1);
+}
+
+int main()
+{
+    std::cout << "dist_v2_cuda\n";
+
+    const float ref = 0.5f;
+    float *in = (float*)calloc(N, sizeof(float));
+    float *out = (float*)calloc(N, sizeof(float));
+
+    for (int i = 0; i < N; i++) {
+        in[i] = scale(i, N);
+    }
+    distanceArray(out, in, ref, N);
+
+#if DEBUG
+    std::cout << std::fixed << std::setprecision(4);
+    for (int i = 0; i < N; i++) {
+        std::cout << "i = " << i << "\tin: " << in[i] << "\tout: " << out[i] << "\n";
+    }
+#endif
+
+    free(in);
+    free(out);
+    return 0;
 }
