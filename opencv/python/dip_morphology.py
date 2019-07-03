9954b7b5dfb125c9438eccfb61265a3cdff17412
Zihan Chen <zihan.chen.jhu@gmail.com>
2019-07-03 09:42:36 -0700

    Added warping and morphology examples

--- opencv/python/dip_morphology.py
+++ opencv/python/dip_morphology.py
@@ -1,29 +1,38 @@
 #!/usr/bin/env python
 
 """
-Shows how to use blue (average)
+Shows how to use basic morphology
+1) erosion
+2) dilation
+3) open
+4) close
 """
 
 import cv2
+import numpy as np
 
 
-img = cv2.imread('../imgs/lena.jpg')
+img = cv2.imread('./data/morphology_j.png')
+kernel = np.ones((5,5), np.uint8)
+erosion = cv2.erode(img, kernel)
 
+# dilation
+dilation = cv2.dilate(img, kernel)
 
-# TODO
-#  1) dilation
-#  2) erosion
-#  3) others
+# opening: erosion followed by dilation
+img_opening = cv2.imread('./data/opening.png', cv2.IMREAD_GRAYSCALE)
+opening = cv2.morphologyEx(img_opening, cv2.MORPH_OPEN, kernel)
 
+# closing: dilation followed by erosion
+img_closing = cv2.imread('./data/closing.png', cv2.IMREAD_GRAYSCALE)
+closing = cv2.morphologyEx(img_closing, cv2.MORPH_CLOSE, kernel)
 
-kernel_size = (5, 5)
-img_blur = cv2.blur(img, kernel_size)
 
 cv2.imshow('default', img)
-cv2.imshow('blur', img_blur)
+cv2.imshow('erosion', erosion)
+cv2.imshow('dilation', dilation)
+cv2.imshow('opening', np.hstack((img_opening, opening)))
+cv2.imshow('closing', np.hstack((img_closing, closing)))
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
---
+++ opencv/python/warping.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Shows how to do affine transform (warping)
+
+Date: 2019-07-03
+"""
+
+from pylab import *
+from scipy import ndimage
+import cv2
+
+
+im = cv2.imread('./data/empire.jpg', cv2.IMREAD_GRAYSCALE)
+H = np.array([[1.4,0.05,-100],[0.05,1.5,-100],[0,0,1]])
+
+# A = H[:2,:2]
+# t = H[:2,2]
+im2 = ndimage.affine_transform(im, H[:2,:2], H[:2,2])
+
+plt.figure(); plt.gray()
+plt.imshow(im)
+plt.figure(); plt.gray()
+plt.imshow(im2)
+plt.show()
