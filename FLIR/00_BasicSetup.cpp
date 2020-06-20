49ad19426913653c25226b6545348ca062a0f11c
Zihan Chen <zihan.chen.jhu@gmail.com>
2020-06-19 18:18:41 -0700

    Added FLIR camera basic example

---
+++ FLIR/00_BasicSetup.cpp
@@ -0,0 +1,40 @@
+/*
+ * Shows how to acquire an image.
+ *
+ * */
+
+
+#include "Spinnaker.h"
+#include <iostream>
+
+using namespace Spinnaker;
+using namespace Spinnaker::GenApi;
+using namespace Spinnaker::GenICam;
+
+
+int main(int /*argc*/, char** /*argv*/)
+{
+    std::cout << "FLIR: Acquisition\n";
+    auto system = System::GetInstance();
+    auto cam_list = system->GetCameras();
+
+    if (cam_list.GetSize() == 0) {
+        std::cerr << "No cameras found\n";
+        cam_list.Clear();
+        system->ReleaseInstance();
+        return -1;
+    } else {
+        std::cout << cam_list.GetSize() << " Cameras Found\n";
+    }
+    auto cam = cam_list.GetByIndex(0);
+    cam->Init();
+    std::cout << "Fancy processing\n";
+    cam->DeInit();
+
+    // Release reference to the camera
+    cam = nullptr;
+    cam_list.Clear();
+    system->ReleaseInstance();
+    return 0;
+}
+
---
+++ FLIR/01_Acquisition.cpp
@@ -0,0 +1,15 @@
+/*
+ * Shows how to acquire an image.
+ *
+ * */
+
+
+#include "Spinnaker.h"
+#include <iostream>
+
+int main(int /*argc*/, char** /*argv*/)
+{
+    std::cout << "FLIR: Acquisition\n";
+
+}
+
