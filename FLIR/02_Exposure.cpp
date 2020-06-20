bca5172fece3db5b2a38d204d49e3018601b33f5
Zihan Chen <zihan.chen.jhu@gmail.com>
2020-06-19 20:40:15 -0700

    Added FLIR exposure example

--- FLIR/02_Exposure.cpp
+++ FLIR/02_Exposure.cpp
@@ -1,75 +1,76 @@
-#!/usr/bin/env python3
-
-"""
-Shows how to manually change camera's exposure time.
-See Exposure_QuickSpin.py example
-
-This program sets camera to different exposure time, then take an
-image. Note the buffer handling mode is set to NewestOnly to make
-sure the image retrieved is taken after the exposure time has been
-changed.
-
-Author: Zihan Chen
-Date: 2019-12-29
-"""
-
-import time
-import PySpin
+/*
+ * Shows how to manually change camera's exposure time.
+ *
+ * */
 
 
-def set_exposure(cam: PySpin.CameraPtr, exposure_ms: int):
-    cam.ExposureAuto.SetValue(PySpin.ExposureAuto_Off)
-    exp_max = cam.ExposureTime.GetMax()
-    if exposure_ms > exp_max:
-        exposure_ms = exp_max
-        print('Exposure set value exceeds limit, setting to max {}'.format(exp_max))
-    elif exposure_ms <= 0:
-        exposure_ms = 1
-        print('Exposure time must be a positive int value')
-    cam.ExposureTime.SetValue(exposure_ms)
-    print('Set exposure to {} ms'.format(exposure_ms))
+#include "Spinnaker.h"
+#include <iostream>
+
+using namespace Spinnaker;
+using namespace Spinnaker::GenApi;
+using namespace Spinnaker::GenICam;
+
+void set_exposure(CameraPtr& cam, int exposure_ms)
+{
+    cam->ExposureAuto.SetValue(ExposureAuto_Off);
+    auto exp_max = cam->ExposureTime.GetMax();
+    if (exposure_ms > exp_max) {
+        exposure_ms = exp_max;
+        std::cerr << "Exposure set value exceeds limit, setting to max " << exp_max << "\n";
+    }
+    else if (exposure_ms <= 0) {
+        exposure_ms = 1;
+        std::cerr << "Exposure time must be a positive int value\n";
+    }
+    cam->ExposureTime.SetValue(exposure_ms);
+    std::cout << "Set exposure to " << exposure_ms << " ms\n";
+}
 
 
-def reset_exposure(cam: PySpin.CameraPtr):
-    try:
-        cam.ExposureAuto.SetValue(PySpin.ExposureAuto_Continuous)
-        print('Automatic exposure enabled...')
-        return True
-    except PySpin.SpinnakerException as e:
-        print('Error: {}'.format(e))
-        return False
+int main(int /*argc*/, char** /*argv*/)
+{
+    std::cout << "FLIR: Acquisition\n";
+    auto system = System::GetInstance();
+    auto cam_list = system->GetCameras();
 
+    if (cam_list.GetSize() == 0) {
+        std::cerr << "No cameras found\n";
+        cam_list.Clear();
+        system->ReleaseInstance();
+        return -1;
+    }
 
-system = PySpin.System_GetInstance()
+    auto cam = cam_list.GetByIndex(0);
+    cam->Init();
+    std::cout << "Fancy processing\n";
+    cam->DeInit();
 
-cam_list = system.GetCameras()
-cam = cam_list.GetByIndex(0)
-cam.Init()
+    printf("*** IMAGE ACQUISITION ***\n");
+    cam->AcquisitionMode.SetValue(AcquisitionMode_Continuous);
+    printf("Acquisition mode set to continuous...");
 
-print('*** IMAGE ACQUISITION ***\n')
-cam.AcquisitionMode.SetValue(PySpin.AcquisitionMode_Continuous)
-print('Acquisition mode set to continuous...')
+    cam->BeginAcquisition();
+    std::vector<int> exposure_all{1000, 2000, 3000, 4000, 5000};
+    for (const auto exposure : exposure_all) {
+        set_exposure(cam, exposure);
+        usleep(50000);
+        auto img = cam->GetNextImage();
+        if (!img->IsIncomplete()) {
+            char file_name[100];
+            sprintf(file_name, "Exposure_%d.jpg", exposure);
+            img->Save(file_name);
+        }
+        img->Release();
+    }
 
-cam.TLStream.StreamBufferHandlingMode.SetValue(PySpin.StreamBufferHandlingMode_NewestOnly)
-print('StreamBuffer handling mode set to NewestOnly...')
+    cam->EndAcquisition();
+    cam->ExposureAuto.SetValue(ExposureAuto_Continuous);
 
-cam.BeginAcquisition()
-cam_serial_number = cam.DeviceSerialNumber.ToString()
+    // Release reference to the camera
+    cam = nullptr;
+    cam_list.Clear();
+    system->ReleaseInstance();
+    return 0;
+}
 
-exposure_all = [1000, 2000, 3000, 4000, 5000]
-
-for exposure in exposure_all:
-    set_exposure(cam, exposure)
-    time.sleep(0.05)
-    img = cam.GetNextImage()
-    if not img.IsIncomplete():
-        img.Save('Exposure_{}.jpg'.format(exposure))
-    img.Release()
-
-cam.EndAcquisition()
-reset_exposure(cam)
-
-cam.DeInit()
-del cam
-cam_list.Clear()
-system.ReleaseInstance()
