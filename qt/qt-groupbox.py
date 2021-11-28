581343a1654f600f6dcbb0f09601317e47d480d4
Zihan Chen <zihan.chen.jhu@gmail.com>
2021-11-28 12:46:04 +0800

    Added qt groupbox example

---
+++ qt/qt-groupbox.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+Qt Groupbox Example
+
+Author: Zihan Chen
+Date: 2021-11-28
+"""
+
+import sys
+from qtpy.QtWidgets import *
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    qgb = QGroupBox("GroupBox")
+    vbox = QVBoxLayout()
+    vbox.addWidget(QPushButton("hello"))
+    vbox.addWidget(QPushButton("world"))
+    qgb.setLayout(vbox)
+    qgb.setMinimumWidth(200)
+    qgb.show()
+    sys.exit(app.exec_())
