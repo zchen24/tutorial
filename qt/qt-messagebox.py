6c411900c79717a27b035910b21e2dd299cbd860
Zihan Chen <zihan.chen.jhu@gmail.com>
2022-07-09 15:41:27 +0800

    Updated qt messagebox example

--- qt/qt-messagebox.py
+++ qt/qt-messagebox.py
@@ -25,6 +25,22 @@ def show_msg_box():
         print("Invalid")
 
 
+def show_msg_box_question():
+    print("Show MessageBox")
+    QMessageBox.question(None, "Question", "This is a question", QMessageBox.Ok)
+
+def show_msg_box_information():
+    QMessageBox.information(None, "Info", "This is an information", QMessageBox.Ok)
+
+
+def show_msg_box_warn():
+    QMessageBox.warning(None, "Warn", "This is a warning", QMessageBox.Ok)
+
+
+def show_msg_box_critical():
+    QMessageBox.critical(None, "Critical", "This is a critical", QMessageBox.Ok)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     w = QWidget()
@@ -32,8 +48,22 @@ if __name__ == '__main__':
     btn = QPushButton("Show message")
     btn.setMinimumWidth(200)
     btn.clicked.connect(show_msg_box)
+
+    btn_ques = QPushButton("Show Question")
+    btn_ques.clicked.connect(show_msg_box_question)
+    btn_info = QPushButton("Show Info")
+    btn_info.clicked.connect(show_msg_box_information)
+    btn_warn = QPushButton("Show Warn")
+    btn_warn.clicked.connect(show_msg_box_warn)
+    btn_crit = QPushButton("Show Critical")
+    btn_crit.clicked.connect(show_msg_box_critical)
+
     vbox = QVBoxLayout()
     vbox.addWidget(btn)
+    vbox.addWidget(btn_ques)
+    vbox.addWidget(btn_info)
+    vbox.addWidget(btn_warn)
+    vbox.addWidget(btn_crit)
     w.setLayout(vbox)
     w.show()
     sys.exit(app.exec_())
