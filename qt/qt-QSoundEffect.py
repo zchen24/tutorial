40501a95328ce253c6d36b1325d1a56294e1dce5
Zihan Chen <zihan.chen.jhu@gmail.com>
2023-01-29 13:51:59 +0800

    Added setLoopCount to QSoundEffect

--- qt/qt-QSoundEffect.py
+++ qt/qt-QSoundEffect.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     se.setSource(QUrl.fromLocalFile(os.path.abspath("./assets/example.wav")))
     print("setMedia done")
     se.play()
+    se.setLoopCount(2)
     se.setVolume(0.5)
     time.sleep(5)
     sys.exit(app.exec_())
