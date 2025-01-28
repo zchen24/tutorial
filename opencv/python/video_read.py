4c05b8adfb577b63d23d9d46b323777c544527b2
Zihan Chen <zihan.chen.jhu@gmail.com>
2025-01-28 20:42:59 +0800

    Add print FPS info in video_read.py

--- opencv/python/video_read.py
+++ opencv/python/video_read.py
@@ -6,8 +6,11 @@ How to read & display an image
 
 import cv2
 
-
 cap = cv2.VideoCapture('./data/flame.avi')
+# print fps of the video
+fps = cap.get(cv2.CAP_PROP_FPS)
+print("Frames per second: {0}".format(fps))
+
 win_name = 'VideoWin'
 cv2.namedWindow(win_name)
 
@@ -19,7 +22,7 @@ while True:
         i += 1
         cv2.imshow(win_name, frame)
 
-        # uncomment if frame by frame play is needed
+        # uncomment if frame-by-frame play is needed
         # print("frame idx: {}".format(i))
         # if cv2.waitKey(0) == ord('q'):
         if cv2.waitKey(50) == ord('q'):
